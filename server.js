4c262363509cdcbba8e3424d351ad91531707b04
natwus <mareussenai@gmail.com>
2024-06-16 12:55:03 -0300

    comentarios

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
+const express = require('express'); //framework pra criar um servidor http
+const nodemailer = require('nodemailer'); //modulo pra enviar os emails
+const cors = require('cors');//modulo para permitor ou restringir solicitação web
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: "gmail",//serviço de email
     auth: {
-        user: 'biotech.pharmaease@gmail.com',
+        user: 'biotech.pharmaease@gmail.com',//user que envia a msg
         pass: 'yslpmnfecfoubrtu',
     }
 });
 
-app.post('/send-email', (req, res) => {
-    const { to, subject, text } = req.body;
+app.post('/send-email', (req, res) => {  
+    const { to, subject, text } = req.body; //recebe as informaçoes da const response no contato.tsx
 
-    transporter.sendMail({
+    transporter.sendMail({ //envia o email
         from: 'biotech.pharmaease@gmail.com',  
         to, 
         subject,
